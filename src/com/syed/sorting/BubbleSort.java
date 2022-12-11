0d8995fef9e627e1228a79bff1d7372244124c99
Hawk Itzme <smquasim016@gmail.com>
2022-12-11 23:36:02 +0530

    BubbleSort code added

---
+++ src/com/syed/sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.syed.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 3, 1, 2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped){
+                break;
+            }
+        }
+    }
+}
