89d4079cbf6a2765c811bc0415b7e289bcb0d009
Hawk Itzme <smquasim016@gmail.com>
2022-12-14 23:53:57 +0530

    Reverse Number code added

---
+++ src/com/syed/RevNumber.java
@@ -0,0 +1,17 @@
+package com.syed;
+
+import java.util.Scanner;
+
+public class RevNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int rnum = 0;
+        while (num>0){
+            int rem = num % 10;
+            num/=10;
+            rnum = rnum * 10 + rem;
+        }
+        System.out.println(rnum);
+    }
+}
