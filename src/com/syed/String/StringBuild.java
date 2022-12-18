d97f02fce84c2ebd9d2d5b210b441d9a533bb2ed
Hawk Itzme <smquasim016@gmail.com>
2022-12-18 23:55:12 +0530

    String Build Code added

---
+++ src/com/syed/String/StringBuild.java
@@ -0,0 +1,12 @@
+package com.syed.String;
+
+public class StringBuild {
+    public static void main(String[] args) {
+        StringBuilder build = new StringBuilder();
+        for (int i = 0; i< 26; i++){
+            char ch = (char)('a' + i);
+            build.append(ch);
+        }
+        System.out.println(build);
+    }
+}
