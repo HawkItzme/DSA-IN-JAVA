8dbae422baefd17a86829ef0fde6d0dc08e7729a
Hawk Itzme <smquasim016@gmail.com>
2022-12-20 23:40:31 +0530

    Insertion Sort with recursion code added

---
+++ src/com/syed/Recursion/InsertionSort.java
@@ -0,0 +1,27 @@
+package com.syed.Recursion;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = {12, 11, 13, 5, 6};
+
+        insertionSort(arr, arr.length);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void insertionSort(int[] arr, int n){
+        if(n<= 1){
+            return;
+        }
+        insertionSort(arr, n -1);
+
+        int last = arr[n-1];
+        int j = n-2;
+        while(j>= 0 && arr[j] > last){
+            arr[j+1] = last;
+            j--;
+        }
+        arr[j + 1] = last;
+    }
+}
