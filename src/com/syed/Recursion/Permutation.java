4c2991679b665ba9ad1422e40b996fb7919a8be6
Hawk Itzme <smquasim016@gmail.com>
2022-12-29 23:57:44 +0530

    Permutation in List code added

--- src/com/syed/Recursion/Permutation.java
+++ src/com/syed/Recursion/Permutation.java
@@ -1,5 +1,7 @@
 package com.syed.Recursion;
 
+import java.util.ArrayList;
+
 public class Permutation {
     public static void main(String[] args) {
         permutation("", "abc");
@@ -15,5 +17,29 @@ public class Permutation {
             String s = p.substring(i, p.length());
             permutation(f + ch + s, up.substring(1) );
         }
+    }
+}
+
+class PermutInList{
+    public static void main(String[] args) {
+
+        System.out.println(permutation("", "abc"));
+
+    }
+    static ArrayList<String> permutation(String p, String up){
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<String>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i, p.length());
+            ans.addAll( permutation(f + ch + s, up.substring(1)));
+
+        }
+        return ans;
     }
 }
