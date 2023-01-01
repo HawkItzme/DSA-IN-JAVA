91d1ba4e1e3236fab8a6becb218fc6725755f2ba
Hawk Itzme <smquasim016@gmail.com>
2023-01-01 19:15:15 +0530

    LL Cycle node code added

--- src/com/syed/LinkedList/CyclicQuestion.java
+++ src/com/syed/LinkedList/CyclicQuestion.java
@@ -14,6 +14,28 @@ public class CyclicQuestion {
         }
         return false;
     }
+
+    //find length of the cycle
+    public int lengthCycle(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                //length calc
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                }while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
 }
 
 class ListNode{
