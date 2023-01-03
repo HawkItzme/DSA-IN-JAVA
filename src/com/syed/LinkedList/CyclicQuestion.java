8b7436f5a5fcb2dddcdc07027181122c3885673b
Hawk Itzme <smquasim016@gmail.com>
2023-01-03 15:39:30 +0530

    Code to find node at the start of cycle added

--- src/com/syed/LinkedList/CyclicQuestion.java
+++ src/com/syed/LinkedList/CyclicQuestion.java
@@ -1,5 +1,7 @@
 package com.syed.LinkedList;
 
+import java.util.List;
+
 public class CyclicQuestion {
     public boolean hasCycle(ListNode head){
         ListNode fast = head;
@@ -35,6 +37,42 @@ public class CyclicQuestion {
             }
         }
         return 0;
+    }
+
+    public  ListNode detectCycle(ListNode head){
+        int length = 0;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0){
+            return null;
+        }
+        // find the start node
+        ListNode f = head;
+        ListNode s = head;
+
+        while (length > 0){
+            s = s.next;
+            length--;
+        }
+
+        // keep moving both forwards and they will meet at the start of cycle
+
+        while (f != s){
+            f = f.next;
+            s = s.next;
+        }
+        return s;
     }
 }
 
