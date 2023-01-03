b41d8b041a8c2975ddf1658bfe69b0d479eadc3a
Hawk Itzme <smquasim016@gmail.com>
2023-01-03 18:08:35 +0530

    Finding middle node code added

--- src/com/syed/LinkedList/CyclicQuestion.java
+++ src/com/syed/LinkedList/CyclicQuestion.java
@@ -74,6 +74,16 @@ public class CyclicQuestion {
         }
         return s;
     }
+    public ListNode middleNode(ListNode head){
+        ListNode s = head;
+        ListNode f = head;
+
+        while(f != null && f.next != null){
+            s = s.next;
+            f = f.next;
+        }
+        return s;
+    }
 }
 
 class ListNode{
