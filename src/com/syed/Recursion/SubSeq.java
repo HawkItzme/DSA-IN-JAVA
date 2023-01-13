904fa14645b131c9254a88e97687a46e25a640a0
Hawk Itzme <smquasim016@gmail.com>
2023-01-13 23:19:35 +0530

    Subseq duplicate remove code added

--- src/com/syed/Recursion/SubSeq.java
+++ src/com/syed/Recursion/SubSeq.java
@@ -64,3 +64,35 @@ class subSetIte{
         return outer;
     }
 }
+
+class DuplicateRemove{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2};
+        List<List<Integer>> ans = subsetDuplicate(arr);
+        for (List<Integer> list : ans){
+            System.out.print(list);
+        }
+    }
+    static List<List<Integer>> subsetDuplicate(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start;
+        int end  = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            start = 0;
+            //if the current and previous element is same
+            if (i>0 && arr[i] == arr[i-1]){
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++){
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
