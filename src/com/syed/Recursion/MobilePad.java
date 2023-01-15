a28b1cc5f42fe7c986b5ed31fb2ab362934c4531
Hawk Itzme <smquasim016@gmail.com>
2023-01-15 20:21:45 +0530

    Mobile pad list code added

--- src/com/syed/Recursion/MobilePad.java
+++ src/com/syed/Recursion/MobilePad.java
@@ -1,5 +1,7 @@
 package com.syed.Recursion;
 
+import java.util.ArrayList;
+
 public class MobilePad {
     public static void main(String[] args) {
         padRet("", "12");
@@ -14,5 +16,25 @@ public class MobilePad {
             char ch = (char) ('a' + i);
             padRet(p + ch, up.substring(1));
         }
+    }
+}
+
+class MobilePadList{
+    public static void main(String[] args) {
+        System.out.println(padRet("", "12"));
+    }
+    static ArrayList<String> padRet(String p, String up){
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        int digit = up.charAt(0) - '0';
+        for (int i = (digit - 1) * 3; i < digit * 3; i++){
+            char ch = (char) ('a' + i);
+            ans.addAll(padRet(p + ch, up.substring(1)));
+        }
+        return ans;
     }
 }
