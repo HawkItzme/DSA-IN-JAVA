a146108ac6097cf7638b93efbabd135244a2c21a
Hawk Itzme <smquasim016@gmail.com>
2023-01-16 23:08:02 +0530

    Mobile Pad 3 code added

--- src/com/syed/Recursion/MobilePad.java
+++ src/com/syed/Recursion/MobilePad.java
@@ -38,3 +38,21 @@ class MobilePadList{
         return ans;
     }
 }
+
+class MobliePadCount{
+    public static void main(String[] args) {
+        System.out.println(padCount("", "12"));
+    }
+    static int padCount(String p, String up){
+        if (up.isEmpty()){
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0) - '0';
+        for (int i = (digit - 1) * 3; i < digit * 3; i++){
+            char ch = (char) ('a' + i);
+            count = count  + padCount(p + ch, up.substring(1));
+        }
+        return count;
+    }
+}
