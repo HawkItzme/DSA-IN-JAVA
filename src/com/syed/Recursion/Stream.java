87b0fb5718420b408a61d820a60b14530cf97166
Hawk Itzme <smquasim016@gmail.com>
2023-01-19 23:53:28 +0530

    Stream 3 code added

--- src/com/syed/Recursion/Stream.java
+++ src/com/syed/Recursion/Stream.java
@@ -34,3 +34,19 @@ class Stream_2{
         }
     }
 }
+
+class Stream_3{
+    public static void main(String[] args) {
+        System.out.println(skip("abcdappleghf"));
+    }
+    static String skip(String up){
+        if (up.isEmpty()){
+            return " ";
+        }
+        if (up.startsWith("apple")){
+            return skip(up.substring(5));
+        }else {
+            return  up.charAt(0) + skip(up.substring(1));
+        }
+    }
+}
