89d51d71bce3790201c92c73f31cdadd056d0621
Hawk Itzme <smquasim016@gmail.com>
2023-01-25 23:51:06 +0530

    Path In list Restrict code added

--- src/com/syed/Recursion/Maze.java
+++ src/com/syed/Recursion/Maze.java
@@ -83,3 +83,35 @@ class pathInListDiag{
         return list;
     }
 }
+
+class pathInListRestrict{
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        } ;
+        System.out.println(path("", board, 0, 0));
+    }
+    static ArrayList<String> path(String p, boolean[][] maze, int r, int c){
+        if (r == maze.length - 1 && c == maze[0].length - 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (!maze[r][c]){
+            return list;
+        }
+
+
+        if (r < maze.length - 1){
+            list.addAll( path(p + 'D', maze,  r + 1, c));
+        }
+        if (c < maze[0].length - 1){
+            list.addAll(path(p + 'R', maze,  r, c + 1));
+        }
+
+        return list;
+    }
+}
