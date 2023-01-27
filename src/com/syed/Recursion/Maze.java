f4779e337a0c1f040ced793835ca240b93154e34
Hawk Itzme <smquasim016@gmail.com>
2023-01-27 22:47:55 +0530

    Path Array code added

--- src/com/syed/Recursion/Maze.java
+++ src/com/syed/Recursion/Maze.java
@@ -1,6 +1,7 @@
 package com.syed.Recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Maze {
     public static void main(String[] args) {
@@ -153,5 +154,52 @@ class BackTracking{
 
         maze[r][c] = true;
         return list;
+    }
+}
+
+class PathArray{
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        } ;
+        int[][] path = new int[board.length][board[0].length];
+        allpath("", board, 0, 0, path, 1);
+    }
+    static void allpath(String p, boolean[][] maze, int r, int c,int[][] path, int step  ){
+        if (r == maze.length - 1 && c == maze[0].length - 1){
+            path[r][c] = step;
+            for (int[] arr : path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return ;
+        }
+
+        if (!maze[r][c]){
+            return;
+        }
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        if (r < maze.length - 1){
+            allpath(p + 'D', maze,  r + 1, c, path, step + 1);
+        }
+        if (c < maze[0].length - 1){
+            allpath(p + 'R', maze,  r, c + 1, path, step + 1);
+        }
+        if (c > 0){
+            allpath(p + 'L', maze,  r, c - 1,path,  step + 1);
+        }
+
+        if (r > 0){
+            allpath(p + 'U', maze,  r - 1, c,path, step + 1 );
+        }
+
+        maze[r][c] = true;
+        path[r][c] = 0;
+
     }
 }
