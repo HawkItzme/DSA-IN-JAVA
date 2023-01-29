a937058443327ba747521aa1d55595fbd89f45da
Hawk Itzme <smquasim016@gmail.com>
2023-01-29 23:21:42 +0530

    Dice 2 code added

--- src/com/syed/Recursion/Dice.java
+++ src/com/syed/Recursion/Dice.java
@@ -1,5 +1,7 @@
 package com.syed.Recursion;
 
+import java.util.ArrayList;
+
 public class Dice {
     public static void main(String[] args) {
         dice("", 4);
@@ -12,5 +14,23 @@ public class Dice {
         for(int i = 1; i <= 6 && i<= target; i++ ){
             dice(p + i, target - i);
         }
+    }
+}
+
+class Dice2{
+    public static void main(String[] args) {
+        System.out.println(dice("", 4));
+    }
+    static ArrayList<String> dice(String p, int target){
+        if (target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 1; i <= 6 && i<= target; i++ ){
+            list.addAll(dice(p + i, target - i));
+        }
+        return list;
     }
 }
