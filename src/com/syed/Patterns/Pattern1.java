b49a42de3b345953c1634af44795525c0c2da694
Hawk Itzme <smquasim016@gmail.com>
2023-02-18 22:55:50 +0530

    Pattern 3 code added

--- src/com/syed/Patterns/Pattern1.java
+++ src/com/syed/Patterns/Pattern1.java
@@ -27,3 +27,16 @@ class Pattern2{
         }
     }
 }
+
+class Pattern3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i<=n; i++){
+            for (int j =1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
