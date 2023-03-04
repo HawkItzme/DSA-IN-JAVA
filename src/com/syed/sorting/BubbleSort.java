a80f48b9ab50cde7c434c83230a63197e93b44d6
Hawk Itzme <smquasim016@gmail.com>
2023-03-05 00:01:03 +0530

    Updated Bubble sort

--- src/com/syed/sorting/BubbleSort.java
+++ src/com/syed/sorting/BubbleSort.java
@@ -9,15 +9,14 @@ public class BubbleSort {
         System.out.println(Arrays.toString(arr));
     }
     static void bubble(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++){
             boolean swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j - 1] > arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
-                    swapped = true;
-                }
+                    swapped = true;}
             }
             if (!swapped){
                 break;
